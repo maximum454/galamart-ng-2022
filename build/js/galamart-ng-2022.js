8b87c656a5c4b9f62cf5b1031a46971d1df37891
Maxim Karachyov <karachyov.m@gmail.com>
2021-11-02 00:32:09 +0600

    fix

--- build/js/galamart-ng-2022.js
+++ build/js/galamart-ng-2022.js
@@ -4,6 +4,26 @@ $(function () {
 
 })
 
+const swiperCollection = new Swiper('.swiper-collection', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-collection-button-next',
+        prevEl: '.swiper-collection-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 4,
+        }
+    }
+});
+
 const swiperVideo = new Swiper('.swiper-video', {
     loop: true,
     slidesPerView: 1,
